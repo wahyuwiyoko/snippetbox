34e6171b4239361f41d63b22db2d6b56aa609f1c
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-02 16:59:51 +0800

    Add `addr` runtime config & use standard streams for logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
@@ -15,7 +23,13 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
 }
