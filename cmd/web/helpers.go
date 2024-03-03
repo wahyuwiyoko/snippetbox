e9d9d964df0feee35bec98917b7bf9b8d49407ed
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-03 16:04:17 +0800

    Fix runtime errors when render template
    Write template into a buffer, if it fails, it will throw an error.
    If it works, write the contents of the buffer to `http.ResponseWriter`.

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -30,9 +31,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 
-	err := ts.Execute(w, td)
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, td)
 
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	buf.WriteTo(w)
 }
