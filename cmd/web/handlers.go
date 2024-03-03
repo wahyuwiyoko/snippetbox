0b196d146492b31a40eaa7f024ec7acdc3ce5790
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-03 09:04:57 +0800

    Add SQL statement execution in insert snippet model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,5 +51,17 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet"))
+	// Dummy data
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
