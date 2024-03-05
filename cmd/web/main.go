6f13a2ffe33d64b6fdd79c0b95115ff07f703869
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-05 13:42:34 +0800

    Add connection timeouts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -78,10 +78,13 @@ func main() {
 	}
 
 	server := &http.Server{
-		Addr:      *addr,
-		ErrorLog:  errorLog,
-		Handler:   app.routes(),
-		TLSConfig: tlsConfig,
+		Addr:         *addr,
+		ErrorLog:     errorLog,
+		Handler:      app.routes(),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
