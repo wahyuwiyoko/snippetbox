09941b65d7595f27de9f753c930cb1ac120c1604
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-06 22:55:48 +0800

    Add signup form and signup authentication

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -3,6 +3,7 @@ package forms
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -11,6 +12,8 @@ type Form struct {
 	url.Values
 	Errors errors
 }
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func New(data url.Values) *Form {
 	return &Form{
@@ -26,6 +29,18 @@ func (f *Form) Required(fields ...string) {
 		if strings.TrimSpace(value) == "" {
 			f.Errors.Add(field, "This field cannot be empty")
 		}
+	}
+}
+
+func (f *Form) MinLength(field string, d int) {
+	value := f.Get(field)
+
+	if value == "" {
+		return
+	}
+
+	if utf8.RuneCountInString(value) < d {
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
 	}
 }
 
@@ -55,6 +70,18 @@ func (f *Form) PermittedValues(field string, opts ...string) {
 	}
 
 	f.Errors.Add(field, "This field is invalid")
+}
+
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
+	value := f.Get(field)
+
+	if value == "" {
+		return
+	}
+
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field, "This field is invalid")
+	}
 }
 
 func (f *Form) Valid() bool {
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -2,8 +2,11 @@ package mysql
 
 import (
 	"database/sql"
+	"strings"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/wahyuwiyoko/snippetbox/pkg/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -11,7 +14,26 @@ type UserModel struct {
 }
 
 func (model *UserModel) Insert(name, email, password string) error {
-	return nil
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = model.DB.Exec(stmt, name, email, string(hashedPassword))
+
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+
+	return err
 }
 
 func (model *UserModel) Authenticate(email, password string) (int, error) {
