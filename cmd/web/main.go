0cc7d4c36fc33f00e2823aa64df5ed2072402568
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-09 02:36:15 +0800

    Add mocks test for database models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,16 +12,25 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golangcollege/sessions"
+	"github.com/wahyuwiyoko/snippetbox/pkg/models"
 	"github.com/wahyuwiyoko/snippetbox/pkg/models/mysql"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
 	session       *sessions.Session
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 type contextKey string
