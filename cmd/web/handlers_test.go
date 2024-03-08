701e95db6febcd3c5939e5e1dbf70259ee06341a
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2024-03-09 02:44:57 +0800

    Add test for `showSnippet` handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -19,5 +20,42 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
+	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+
+	defer ts.Close()
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			code, _, body := ts.get(t, test.urlPath)
+
+			if code != test.wantCode {
+				t.Errorf("want %d; got %d", test.wantCode, code)
+			}
+
+			if !bytes.Contains(body, test.wantBody) {
+				t.Errorf("want body to contain %q", test.wantBody)
+			}
+		})
 	}
 }
